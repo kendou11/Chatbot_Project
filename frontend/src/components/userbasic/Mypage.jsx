0c5944343c9dce72e0d08efb693a3ca9860e0e91
Lsangsu <sangsu218@naver.com>
2025-12-22 12:07:21 +0900

    1222_1207_LSS

--- frontend/src/components/userbasic/Mypage.jsx
+++ frontend/src/components/userbasic/Mypage.jsx
@@ -12,15 +12,46 @@ const Mypage = () => {
 
                     <Col xs={12} md={8}>
                         <Form.Label>닉네임 변경</Form.Label>
-                        <InputGroup className="mb-1">
+                        <InputGroup className="mb-4">
                             <Form.Control
                                 placeholder="Recipient's username"
                                 aria-describedby="basic-addon2"
+                                style={{ maxWidth: '250px' }}
                             />
                             <Button variant="outline-secondary" id="button-addon2">
                                 Button
                             </Button>
                         </InputGroup>
+                        
+                        <div className='mypage-moneybox'>
+                            <div className='mypage-money'>현재 잔액</div>
+                            <Button variant="primary">Primary</Button>
+
+                        </div>
+
+                    </Col>
+                </Row>
+
+                <Row >
+                    <Col xs={12} md={6}>
+                        <div>
+                            <h3>개인비서</h3>
+                            <Row>
+                            <div className="membership_circle_div" style={{justifyContent:'space-around', paddingBottom:'0px'}}>
+                                <Image src="/img/membership_img.png" roundedCircle className="w-25 mx-1" />
+                                <Image src="/img/membership_img.png" roundedCircle className="w-25 mx-1" />
+                            </div>
+                            </Row>
+                            <Row>
+                            <div className="membership_circle_div" style={{justifyContent:'space-around'}}>
+                                <Image src="/img/membership_img.png" roundedCircle className="w-25 mx-1" />
+                                <Image src="/img/membership_plus_img.png" roundedCircle className="w-25 mx-1" />
+                            </div>
+                            </Row>
+                        </div>
+                    </Col>
+
+                    <Col xs={12} md={6}>
                         <Form.Label>비밀번호 변경</Form.Label>
                         <InputGroup className="mb-1">
                             <Form.Control
@@ -39,13 +70,9 @@ const Mypage = () => {
                                 Button
                             </Button>
                         </InputGroup>
-
+                        <Button variant="danger" className="mt-2">회원탈퇴</Button>
+                        <Button variant="danger" className="mx-3 mt-2" disabled>멤버십 해지하기</Button>
                     </Col>
-                </Row>
-
-                <Row >
-                    <Col xs={6}>xs=6</Col>
-                    <Col xs={6}>xs=6</Col>
                 </Row>
             </Container>
         </div>
